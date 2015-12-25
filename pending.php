542f820c7c948de2a1d1c24fe3124cb249285ca6
mohamed <mohamed_moanis@outlook.com>
2015-12-25 14:25:44 +0200

    pending page

--- pending.php
+++ pending.php
@@ -1,7 +1,20 @@
-<?php include ("templates/headr.php"); ?>
-<?php include ("templates/nav.php"); ?>
-<html>
-Put Body
-</html>
+<?php
+    $header_tag_extras = "";
+    $header_section_styles = "";
+    $header_section_metas = "";
+    $header_section_scripts = "";
+    $header_section_extras = "";
+    $body_tag_extras = "";
 
-<?php include ("templates/footer.php"); ?>
+    $body_section_styles = "";
+    $body_section_scripts = "";
+    $body_section_content = "<p1>Your request is pending, please contact the site Admin to approve your request</p1>";
+
+    $navbar_content = array(
+        array("index.php", "Home"),
+        array("about.php", "About us."),
+        array("contact.php", "contact us.")
+    );
+
+    include ("templates/base.php");
+?>
