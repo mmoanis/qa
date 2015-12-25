636f9357240d08b8bb13d8dd2c58f061384322ea
mohamed <mohamed_moanis@outlook.com>
2015-12-25 14:04:01 +0200

    introduced base template

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-<?php include ("templates/headr.php"); ?>
-<?php include ("templates/nav.php"); ?>
-<html>
-Home Body
-</html>
+<?php
+    $header_tag_extras = "";
+    $header_section_styles = "";
+    $header_section_metas = "";
+    $header_section_scripts = "";
+    $header_section_extras = "";
+    $body_tag_extras = "";
 
-<?php include ("templates/footer.php"); ?>
+    $body_section_styles = "";
+    $body_section_scripts = "";
+    $body_section_content = "";
+
+    include ("templates/base.php");
+?>
---
+++ templates/base.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <header <?php echo $header_tag_extras ?>>
+        <title> Cairo University, Faculty of Engineering. </title>
+        <?php echo $header_section_styles ?>
+        <?php echo $header_section_metas ?>
+        <?php echo $header_section_scripts ?>
+        <?php echo $header_section_extras ?>
+    </header>
+
+    <body <?php echo $body_tag_extras ?> >
+        <!-- Header -->
+        <?php include ("templates/headr.php"); ?>
+        <!-- Header Ends -->
+
+        <!-- Nav Bar -->
+        <?php include ("templates/nav.php"); ?>
+        <!-- Nav Bar Ends -->
+
+        <?php echo $body_section_styles ?>
+        <?php echo $body_section_scripts ?>
+
+        <!-- Content -->
+        <?php echo $body_section_content ?>
+        <!-- Content Ends -->
+
+        <!-- Footer -->
+        <?php include ("templates/footer.php"); ?>
+        <!-- Footer Ends -->
+    </body>
+</html>
