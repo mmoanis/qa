3088477c66d3e3c77255c2f55b83df1cbfe26089
mohamed <mohamed_moanis@outlook.com>
2015-12-26 16:39:36 +0200

    department manager homepage

---
+++ department-managment/index.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['user_id']) || strcmp($_SESSION['type'], 'department_manager') != 0 )
+    {
+        // redirect unauthorized user at once to homepage
+        header('Location: http://localhost/qa/index.php');
+        die();
+    }
+
+    $header_tag_extras = "";
+    $header_section_styles = "";
+    $header_section_metas = "";
+    $header_section_scripts = "";
+    $header_section_extras = "";
+    $body_tag_extras = "";
+
+    $body_section_styles = "";
+    $body_section_scripts = "";
+
+    // TODO: fill department_manager content when database is ready
+    $body_section_content = '';
+
+    $navbar_content = array(
+        array("../index.php", "Home"),
+        array("new-course.php", "Add new course."),
+        array("../about.php", "About us."),
+        array("../contact.php", "contact us."),
+        array("../logout.php", "Sign out.")
+    );
+
+
+    include ("../templates/base.php");
+?>
