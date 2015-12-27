5dc3669fb3b086e4eff9fed9f2fb6c62d4e4e885
mohamed <mohamed_moanis@outlook.com>
2015-12-27 21:32:56 +0200

    allow file uploads

--- instructor/course.php
+++ instructor/course.php
@@ -26,7 +26,7 @@
     $files = getFilesByCourseID($_REQUEST['page_course_id']);
 
     $body_section_content = '<h1> Information About Course: </h1><p>You can add links to files, remove links or update links</p>';
-    $body_section_content .= '<form method="POST" action="updateFile.php">
+    $body_section_content .= '<form method="POST" action="upload.php">
         ID: <input type="text" readonly name="ID" value="'. $course['ID']  .'"><br>
         Name: <input type="text" readonly name="name" value="'.$course['name'].'"><br>
         Code: <input type="text" readonly name="code" value="'.  $course['code'] .'"><br>
@@ -37,9 +37,11 @@
     {
         $body_section_content .= str_replace("_", " ", $file['type']) .': <input type="text" name="'.$file['type'].'" value="';
         if ($file['data'] == NULL)
-            $body_section_content .= '" placeholder="Insert Link to file."/><br>';
+            $body_section_content .= '" placeholder="Insert Link to file."/>';
         else
-            $body_section_content .= $file['data'] . '"/><br>';
+            $body_section_content .= $file['data'] . '"/>';
+
+        $body_section_content .= '<input type="file" name="upload_'.$file['type'].'" /><br>';
     }
     $body_section_content .= '<button type="submit" value="confirm"  onclick="return confirm(\'Are you sure?\')">Confirm</button>
         </form>';
---
+++ instructor/upload.php
@@ -0,0 +1,40 @@
+<?php
+	session_start();
+
+    if (!isset($_SESSION['user_id']) || strcmp($_SESSION['type'], 'instractor') != 0  || !isset($_POST['ID']))
+    {
+        // redirect unauthorized user at once to homepage
+        header('Location: http://localhost/qa/index.php');
+        die();
+    }
+
+    $header_tag_extras = "";
+    $header_section_styles = "";
+    $header_section_metas = "";
+    $header_section_scripts = "";
+    $header_section_extras = "";
+    $body_tag_extras = "";
+
+    $navbar_signup_login = "";
+
+    $body_section_styles = "";
+    $body_section_scripts = "";
+
+    require('../database/models.php');
+    // update course files
+    $files = getFilesByCourseID($_POST['ID']);
+    foreach ($files as $file)
+    {
+        updateFileData($file['ID'],$_POST[$file['type']]);
+    }
+
+    $body_section_content = '<p>Please wait while your request is completed....</p>';
+
+    $navbar_signup_login = false;
+    $navbar_content = array();
+
+
+    include ("../templates/base.php");
+    header('Location: http://localhost/qa/index.php');
+    die();
+?>
