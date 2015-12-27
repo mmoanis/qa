202ab80373d11c6f5e3e215f4d1906e364648e36
mohamed <mohamed_moanis@outlook.com>
2015-12-27 23:49:50 +0200

    files uploads

--- instructor/upload.php
+++ instructor/upload.php
@@ -8,33 +8,65 @@
         die();
     }
 
-    $header_tag_extras = "";
-    $header_section_styles = "";
-    $header_section_metas = "";
-    $header_section_scripts = "";
-    $header_section_extras = "";
-    $body_tag_extras = "";
-
-    $navbar_signup_login = "";
-
-    $body_section_styles = "";
-    $body_section_scripts = "";
-
     require('../database/models.php');
-    // update course files
-    $files = getFilesByCourseID($_POST['ID']);
+
+    $course = getInfoByCourseID($_POST['ID']);
+    $files  = array('upload_Course_Specifications', 'upload_Materials_&_Labs', 'upload_Assignments_&_Project_Documents',
+        'upload_Midterm_Exam', 'upload_Final_Exam', 'upload_End_of_Course_Report');
+
+    $db_files = getFilesByCourseID($_POST['ID']);
+
+    // base directory
+    $target_dir = "../uploads/" . $course['code'];
+    $uploadOk = 1;
+
     foreach ($files as $file)
     {
-        updateFileData($file['ID'],$_POST[$file['type']]);
+        if (empty($_FILES[$file]["name"]))
+        {
+            // no file to upload, update file link only
+            // update course files
+            
+            foreach ($files as $file)
+            {
+                updateFileData($file['ID'],$_POST[$file['type']]);
+            }
+        }
+        else
+        {
+            $target_file = $target_dir . basename($_FILES[$file]["name"]);
+
+            // Check if file already exists
+            if (file_exists($target_file) {
+                echo "Sorry, file already exists.";
+                $uploadOk = 0;
+            }
+
+            // Check file size
+            if ($_FILES[$file]["size"] > 500000) {
+                echo "Sorry, your file is too large.";
+                $uploadOk = 0;
+            }
+
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.";
+            } 
+            else 
+            {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
+                {
+                    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                } 
+                else
+                {
+                    echo "Sorry, there was an error uploading your file.";
+                }
+            }
+        }
+        
     }
-
-    $body_section_content = '<p>Please wait while your request is completed....</p>';
-
-    $navbar_signup_login = false;
-    $navbar_content = array();
-
-
-    include ("../templates/base.php");
+    
     header('Location: http://localhost/qa/index.php');
     die();
 ?>
