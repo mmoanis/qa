1ab6d1e5dc68ce4c791704cf02a334511f5b51fd
mohamed <mohamed_moanis@outlook.com>
2015-12-27 17:23:33 +0200

    adjust list foreach loop

--- instructor/course.php
+++ instructor/course.php
@@ -20,26 +20,26 @@
     $body_section_scripts = "";
 
     $navbar_signup_login = "";
-    require('../database/selectQuires.php');
+    require('../database/models.php');
     // get all files
     $course = getInfoByCourseID($_REQUEST['page_course_id']);
     $files = getFilesByCourseID($_REQUEST['page_course_id']);
 
     $body_section_content = '<h1> Information About Course: </h1><p>You can add links to files, remove links or update links</p>';
-    $body_section_content .= '<form action="POST" action="upload.php">
+    $body_section_content .= '<form method="POST" action="upload.php">
         ID: <input type="text" readonly name="ID" value="'. $course['ID']  .'"><br>
         Name: <input type="text" readonly name="name" value="'.$course['name'].'"><br>
         Code: <input type="text" readonly name="code" value="'.  $course['code'] .'"><br>
-        Semester: <input type="text" readonly name="semester" value="'. $course['semstr'] .'"><br>
+        Semester: <input type="text" readonly name="semester" value="'. $course['semster'] .'"><br>
         Year: <input type="text" readonly name="year" value="'. $course['year'] .'"><br>';
     // add information about the 6 files of the Course
-    for ($i=0; $i < 6; $i++)
+    foreach ($files as $file)
     {
-        $body_section_content .= $files[$i]['type'] .': <input type="text" name="'.$files[$i]['type'].'" value="';
-        if ($files[$i]['data'] == NULL)
-            $body_section_content .= '"/><br>'
+        $body_section_content .= $file['type'] .': <input type="text" name="'.$file['type'].'" value="';
+        if ($file['data'] == NULL)
+            $body_section_content .= '" placeholder="Insert Link to file."/><br>';
         else
-            $body_section_content .= $files[$i]['data'] . '"/><br>'
+            $body_section_content .= $file['data'] . '"/><br>';
     }
     $body_section_content .= '<button type="submit" value="confirm"  onclick="return confirm(\'Are you sure?\')">Confirm</button>
         </form>';
